d32a04e84d0d854d15778f818d77ddca6e407d95
antlen <antlen@users.noreply.github.com>
2021-11-23 22:25:50 +0900

    update version

--- src/main/java/com/coinbase/application/client/CbClientWrapper.java
+++ src/main/java/com/coinbase/application/client/CbClientWrapper.java
@@ -15,7 +15,7 @@ public class CbClientWrapper {
         CoinbaseClientBuilder builder = new CoinbaseClientBuilder(apiKey, secret);
         //default is 25 records per request. Will up to 100 for this cmdline app
         builder.setPaginationLimit(100);
-        client = builder.create();
+        client = builder.build();
         return client;
     }
 
